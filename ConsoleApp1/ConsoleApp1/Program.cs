08a1aed9c5b9b30ffb9a50002181e175d71f18b2
Wiktoria Gut <s25461@pjwstk.edu.pl>
2024-03-17 20:06:04 +0100

    Modyfikacja 1: dodawanie

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            int a = 42;
+            int b = 119;
+            int c = a + b;
+            Console.WriteLine(c);
         }
     }
 }
