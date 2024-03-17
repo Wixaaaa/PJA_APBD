0d8e03e5f9a04acde1fd40a79dd1fe943327e63d
Wiktoria Gut <s25461@pjwstk.edu.pl>
2024-03-17 20:10:13 +0100

    Modyfikacja 3: opuszczanie programu

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -15,7 +15,10 @@
             var name = Console.ReadLine();
             var currentDate = DateTime.Now;
             Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
-            
+
+            Console.Write($"{Environment.NewLine}Press any key to exit...");
+            Console.ReadKey(true);
+
         }
     }
 }
