a06db669277b3a0f7a5a117dd761ef16f392e5d9
Wiktoria Gut <s25461@pjwstk.edu.pl>
2024-03-17 20:08:38 +0100

    Modyfikacja 2: powitanie uzytkownika

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -5,10 +5,17 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            
             int a = 42;
             int b = 119;
             int c = a + b;
             Console.WriteLine(c);
+
+            Console.WriteLine("What is your name?");
+            var name = Console.ReadLine();
+            var currentDate = DateTime.Now;
+            Console.WriteLine($"{Environment.NewLine}Hello, {name}, on {currentDate:d} at {currentDate:t}!");
+            
         }
     }
 }
