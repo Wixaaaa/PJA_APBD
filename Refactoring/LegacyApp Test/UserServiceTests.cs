22d04eaa498e69f3ae26c0dbcba0f2ba35382404
Wiktoria Gut <s25461@pjwstk.edu.pl>
2024-03-24 20:24:05 +0100

    Refactoring Initial Project

---
+++ Refactoring/LegacyApp Test/UserServiceTests.cs
@@ -0,0 +1,23 @@
+﻿using LegacyApp;
+
+namespace LegacyApp_Test
+{
+    internal class UserServiceTests
+    {
+        [Fact]
+        public void AddUser_Should_Return_False_When_Missing_FirstName()
+        {
+            //Arange
+            var service = new UserService();
+
+            //Act
+            var result -service.AddUser(null, null, "kowalksi@wp.pl", new DateTime(1980, 1, 1), 1);
+
+            //Assert
+            Assert.False(result);
+        }
+        [Fact]
+
+
+    }
+}
